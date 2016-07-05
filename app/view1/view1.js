a5d23c3981cb5cdbe7b1a4d7112dc7b8ad9de16b
Fangming <fachai@cisco.com>
2016-07-04 22:09:51 -0700

    Add spring as server code

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -11,10 +11,16 @@ var app=angular.module('myApp.view1', ['ngRoute'])
 
 app.controller('View1Ctrl', function($scope) {
     $scope.testname= "test name";
+    
 
     $scope.printscope=function(){
             console.log($scope);
             return "test function";
+        };
+
+    $scope.testdatabase=function(){        
+            console.log($scope);
+            return "test database";
         };
 
 });
